17db509996317dd7efd04bafe25b27a8a7ff7bf6
Alex Schulz <schubz94@gmail.com>
2023-12-27 17:17:40 +0100

    layout for Login done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,8 @@
-import {Box, Button, TextField, Typography} from "@mui/material";
 import {useState} from "react";
+import { Link } from "react-router-dom";
 import useAxios from "../hooks/useAxios.tsx";
 
-const Login = () => {
+export default function Login() {
 
     const http = useAxios();
 
@@ -39,8 +39,71 @@ const Login = () => {
 
     return (
 
-        <h1 className="text-xlarge text-text-light">LOGIN</h1>
+            <div className="w-full h-screen flex flex-col justify-center items-center bg-border p-8 rounded-xl">
+                <img src="/src/assets/Logo-icon.svg"
+                     alt="Workbench Logo"
+                     className="w-[80px] h-[80px] mx-auto"
+                />
+                <form className="space-y-6 w-full max-w-[400px]"
+                      action="#"
+                      method="POST">
+                    <div>
+                        <label htmlFor="email"
+                               className="block text-sm md:text-medium font-medium leading-6 text-text-light">
+                            Email
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                placeholder="example@mail.com"
+                                autoComplete="email"
+                                className="block w-full rounded-md border-0 py-1.5 font-semibold text-body-bg-hover placeholder:text-text-gray focus:ring-2 focus:ring-inset focus:ring-accent text-small"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password"
+                                   className="block text-sm md:text-medium font-semibold leading-6 text-text-light">
+                                Password
+                            </label>
+                            {/*<div className="text-sm">*/}
+                            {/*    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">*/}
+                            {/*        Forgot password?*/}
+                            {/*    </a>*/}
+                            {/*</div>*/}
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                placeholder="xamplePassWord*0815*_!"
+                                autoComplete="current-password"
+                                className="block w-full rounded-md border-0 py-1.5 font-semibold text-body-bg-hover placeholder:text-text-gray focus:ring-2 focus:ring-inset focus:ring-accent text-small"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-accent px-3 py-1.5 text-sm font-semibold leading-6 text-text-light shadow-sm"
+                        >
+                            Sign in
+                        </button>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <h2 className=" text-small">Not registered yet?</h2>
+                        <Link to="/register">
+                        <p className="text-accent text-small border border-accent">Create Account here</p>
+                        </Link>
+                    </div>
+                </form>
+            </div>
     )
 }
 
-export default Login
